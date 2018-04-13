7c7d00a972bf3381a90e775fbe820bdac10f063e
BuildTools <unconfigured@null.spigotmc.org>
2018-04-13 21:15:59 +0200

    hj

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
+var scroll_pos = 0;
+var bar_width = 0;
+
+jQuery(window).scroll(function () {
+    scroll_pos = $(this).scrollTop();
+    if (jQuery(window).scrollTop() < 0) {
+        scroll_pos = 0 - 0 * scroll_pos;
+
+    }
+    else {
+        bar_width = 0 + 0.62 * scroll_pos;
+    }
+    jQuery(".underlay").width(bar_width);
+
+});
+
+AOS.init();
+
 $(document).ready(function () {
-    //  INDEX PARALLAX
+    //  INDEX PARALLAX ${scroll_color}
     $(".paroller, [data-paroller-factor]").paroller({
         factor: 0.6,
         type: 'background',
         direction: 'vertical'
     });
-    $(function() {
+    /*$(function() {
         $.scrollify({
           section : ".snap",
           scrollSpeed: 1500,
         });
-      });
+      });*/
 });
