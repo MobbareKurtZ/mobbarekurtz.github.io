9b927010168660aea746930b646526bf9a4dfc58
BuildTools <unconfigured@null.spigotmc.org>
2018-04-16 19:16:50 +0200

    harkl

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var scroll_pos = 0;
+/*var scroll_pos = 0;
 var bar_width = 0;
 
 jQuery(window).scroll(function () {
@@ -12,22 +12,32 @@ jQuery(window).scroll(function () {
     }
     jQuery(".underlay").width(bar_width);
 
-});
+});*/
 
 $(document).ready(function () {
     //  TITLE GLITCH
-    $('.glitch').glitch({ minint: 1, maxint: 5, maxglitch: 15, hshift: 5, vshift: 10, direction: 'random' });
+    //$('.glitch').glitch({ minint: 1, maxint: 5, maxglitch: 15, hshift: 5, vshift: 10, direction: 'random' });
     //  TITLE CHANGE
-    var quotes = ["TITLE1","TITLE2","TITLE3","TITLE4"];
-    var i = 0;
-    function loop() {
-        if (i > 3) i = 0;
-        $('.title').text(quotes[i]);
-        i++;
-        setTimeout(loop, 15000);
-    }
-    loop();
-    
+    $(function () {
+        i = 0;
+        texts = ["TITLE2", "TITLE3", "TITLE4", "TITLE1"];
+        setInterval(function () {
+            $('.title').text(texts[i % texts.length]);
+            i++;
+        }, 15000);
+    });
+
+    $(function () {
+        $(window).scroll(function () {
+            var vh = $(window).height();
+            var vw = $(window).width();
+            var scroll_pos = $(this).scrollTop();
+            var newSize = scroll_pos*(vw/(vh-60));
+            console.log(newSize)
+            $(".underlay").width(newSize);
+        });
+    });
+
     //  INDEX PARALLAX
     /*$(".paroller, [data-paroller-factor]").paroller({
         factor: 0.6,
@@ -35,7 +45,7 @@ $(document).ready(function () {
         direction: 'vertical'
     });*/
 
-    
+
 
 
 
