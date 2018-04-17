b883d80a12629f7ef17849d937dd9b67ed902e18
BuildTools <unconfigured@null.spigotmc.org>
2018-04-17 17:36:06 +0200

    ajsk

--- js/script.js
+++ js/script.js
@@ -1,14 +1,54 @@
+//  CLICK TOGGLE
+(function ($) {
+    $.fn.clickToggle = function (func1, func2) {
+        var funcs = [func1, func2];
+        this.data('toggleclicked', 0);
+        this.click(function () {
+            var data = $(this).data();
+            var tc = data.toggleclicked;
+            $.proxy(funcs[tc], this)();
+            data.toggleclicked = (tc + 1) % 2;
+        });
+        return this;
+    };
+}(jQuery));
+
 $(document).ready(function () {
     //  TITLE GLITCH
-    $('.glitch').glitch({ minint: 1, maxint: 5, maxglitch: 15, hshift: 5, vshift: 10, direction: 'random' });
+    $('.glitch').glitch({
+        minint: 1,
+        maxint: 5,
+        maxglitch: 15,
+        hshift: 5,
+        vshift: 10,
+        direction: 'random'
+    });
     //  NAVBAR SCROLL WIDTH
     $(function () {
         $(window).scroll(function () {
             var vw = $(window).width();
             var pos = $('#in2').offset().top;
-            var scroll_pos = $(this).scrollTop();
-            var newSize = scroll_pos * (vw / (pos - 60));
+            var scrollPos = $(this).scrollTop();
+            var newSize = scrollPos * (vw / (pos - 60));
             $(".underlay").width(newSize);
         });
+    });
+    var vh = $(window).height();
+    $('.menuic').clickToggle(function () {
+        var vw = $(window).width();
+        $('.underlay').addClass('ulfull');
+        if ($(".underlay").width() > vw) {
+            $('.nav').addClass('navdown');
+        } else {
+            setTimeout(function () {
+                $('.nav').toggleClass('navdown');
+            }, 100);
+        }
+    }, function () {
+        var vw = $(window).width();
+        $('.nav').removeClass('navdown');
+        setTimeout(function () {
+            $('.underlay').removeClass('ulfull');
+        }, 400);
     });
 });
